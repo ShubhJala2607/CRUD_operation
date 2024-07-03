32053ab37a07a13099e75db02ce278c436d90da8
ShubhJala2607 <shubh.jala@dcmail.ca>
2024-07-03 13:50:39 -0400

    Update streamlit_app.py
    Intialize the redirect

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,14 @@
-# streamlit_app.py
-
 import streamlit as st
 import subprocess
+import time
+import webbrowser
 
 # Function to start the Flask app
 def start_flask():
     subprocess.Popen(["python", "flask_app.py"])
+    time.sleep(5)
+    # redirect to a web browser to the Flask app URL
+    webbrowser.open_new_tab("http://127.0.0.1:5000")
 
 # Display a message to start the Flask app
 st.title("Flask App on Streamlit Cloud")
